f8fd99fa4676d6d9fb22d5233f1ec50867f5251b
Schwelle76 <mattes.zens@web.de>
2022-07-21 18:52:20 +0200

    positioned buttons and started styling

--- scroll-buttons.js
+++ scroll-buttons.js
@@ -2,16 +2,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 const slideRightButton = document.getElementById('slideRight');
 const slideLeftButton = document.getElementById('slideLeft');
-const images = document.getElementsByClassName("images");
+const slideList = document.getElementsByClassName("projectSlideList")[0];
+const projectSlides = document.getElementsByTagName("li");
+
+let centralSlide = 0;
 
 
 slideRightButton.onclick = function() {
-    console.log('slideRightButton clicked');
-    images[0].scrollLeft += 1500;
+    console.log(projectSlides.length);
+    centralSlide += 1;
+    if (centralSlide >= projectSlides.length)
+        centralSlide = 0;
+    slideList.scrollLeft = 1500 * centralSlide + 50;
+    
 }
 
 slideLeftButton.onclick = function() {
-    images[0].scrollLeft -= 1500;
+    centralSlide -= 1;
+    if (centralSlide < 0)
+    centralSlide = projectSlides.length;
+    slideList.scrollLeft = 1500 * centralSlide + 50;
 }
-
 });
