32f149bd34123e1ca590fc046131997856b21e01
Schwelle76 <mattes.zens@web.de>
2022-07-28 09:46:57 +0200

    attention text now animating properly

--- attention-text.js
+++ attention-text.js
@@ -1,37 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
     
     const text = document.getElementsByClassName("attentionText");
-    const delayBetweenIterations = 10000;
+    const delayBetweenIterations = 5000;
     const delayBetweenLetters = 50;
 
+    
     for(let i = 0; i < text.length; i++) {
 
+        callAnimationWithDelay(i);
         setInterval(function() {
-                const letter = text[i];
-                letter.classList.remove("attentionText");
-                void letter.offsetWidth;
-                letter.classList.add("attentionText");
-   
-        }, delayBetweenIterations + delayBetweenLetters * i);
+            callAnimationWithDelay(i);
+        }, delayBetweenIterations);
     }
 
-    /*
-    setInterval(function() {
-        
-        let delayForNextLetter = 0;
-
-        for(letter of text){
-
-            letter.classList.remove("attentionText");
-            void letter.offsetWidth;
-            letter.classList.add("attentionText");
-
-            letter.style.animationDelay = delayForNextLetter;
-            delayForNextLetter += delayBetweenLetters; 
-        };
-    }, 2000);
-
-    */
+    function callAnimationWithDelay(elementIndex){
+        const letter = text[elementIndex];
+        letter.classList.remove("attentionText");
+        void letter.offsetWidth;
+        letter.classList.add("attentionText");
+        letter.style.animationDelay = delayBetweenLetters * elementIndex +"ms";
+    }
 
     
     
