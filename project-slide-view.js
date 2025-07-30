add1f6cd7e7ca1302a938a79112da0c51a1e7e6f
Mattes <mattes.zens@web.de>
2025-07-30 18:13:13 +0200

    Animated project description under project slideshow. Slideshow and description html elements are now generated based on projects.js.

--- project-slide-view.js
+++ project-slide-view.js
@@ -1,76 +1,85 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-const slideRightButton = document.getElementById('slideRight');
-const slideLeftButton = document.getElementById('slideLeft');
-const slideList = document.getElementsByClassName("projectSlideList")[0];
-const projectSlides = document.getElementsByTagName("li");
-
-const maxSlidesImgsHeight = 500;
 
 
-function slidesImg(slideIndex){
-    return projectSlides[slideIndex].getElementsByTagName("img")[0];
-}
+document.addEventListener("DOMContentLoaded", function (event) {
 
-const SlideDistanceToCenterForZeroHeight = 1000;
-function projectSlideCenter(slideIndex){
-    return averageProjectSlideWidth() * slideIndex;
-}
-const centralSlideIndex = () => {
-    return Math.round(slideList.scrollLeft / averageProjectSlideWidth())
-}
+    const slideRightButton = document.getElementById('slideRight');
+    const slideLeftButton = document.getElementById('slideLeft');
+    const slideList = document.getElementsByClassName("projectSlideList")[0];
+    const projectSlides = document.getElementsByTagName("li");
+    const projectDescription = document.getElementsByClassName("projectDescription");
 
-const averageProjectSlideWidth = () =>{
-    let totalProjectSlideWidth = 0;
-    for(let i = 0; i < projectSlides.length; i++){
-        const projectSlideStyle = getComputedStyle(projectSlides[i]);
-        totalProjectSlideWidth += parseFloat(projectSlideStyle.width);
+    const maxSlidesImgsHeight = 500;
+    const SlideDistanceToCenterForZeroHeight = () => window.innerWidth * .8;
+
+
+
+
+    function slidesImg(slideIndex) {
+        return projectSlides[slideIndex].getElementsByTagName("img")[0];
     }
-
-    return totalProjectSlideWidth / projectSlides.length;
-}
-
-slideRightButton.onclick = function() {
     
 
-    let nextSlideIndex = centralSlideIndex() + 1;
-    if (nextSlideIndex >= projectSlides.length)
-        nextSlideIndex = 0;
-
-    slideList.scrollLeft = projectSlideCenter(nextSlideIndex);
-    
-}
-
-slideLeftButton.onclick = function() {
-
-    let nextSlideIndex = centralSlideIndex() - 1;
-    if (nextSlideIndex < 0)
-        nextSlideIndex = projectSlides.length - 1;
-
-    slideList.scrollLeft = projectSlideCenter(nextSlideIndex);
-
-}
-
-function fadeAndGrowSlidesIntoCenter(){
-    for(let i = 0; i < projectSlides.length; i++){
-
-        const slideLowerPositionForZeroHeight = projectSlideCenter(i) - SlideDistanceToCenterForZeroHeight;
-        const slideUpperPositionForZeroHeight = projectSlideCenter(i) + SlideDistanceToCenterForZeroHeight;
-
-        const unlerp = (min, max, value) => (value - min) / (max - min);
-
-        const positionBetweenMaxAndMinHeightInPercent = unlerp(slideLowerPositionForZeroHeight, slideUpperPositionForZeroHeight, slideList.scrollLeft);
-        const t = positionBetweenMaxAndMinHeightInPercent;
-        const imgAwayFromItsCenterInPercent = 2 * Math.min(t, 1 - t);
-
-        //slidesImg(i).style.height = maxSlidesImgsHeight * imgAwayFromItsCenterInPercent;
-        slidesImg(i).style.transform = "scale("+imgAwayFromItsCenterInPercent+")";
-        
-        slidesImg(i).style.opacity = imgAwayFromItsCenterInPercent;
+    function projectSlideCenter(slideIndex) {
+        return averageProjectSlideWidth() * slideIndex;
     }
-}
+    const centralSlideIndex = () => {
+        return Math.round(slideList.scrollLeft / averageProjectSlideWidth())
+    }
 
-setInterval(function(){
-    fadeAndGrowSlidesIntoCenter();
-}, 10);
+    const averageProjectSlideWidth = () => {
+        let totalProjectSlideWidth = 0;
+        for (let i = 0; i < projectSlides.length; i++) {
+            const projectSlideStyle = getComputedStyle(projectSlides[i]);
+            totalProjectSlideWidth += parseFloat(projectSlideStyle.width);
+        }
+
+        return totalProjectSlideWidth / projectSlides.length;
+    }
+
+
+    slideRightButton.onclick = function () {
+
+
+        let nextSlideIndex = centralSlideIndex() + 1;
+        if (nextSlideIndex >= projectSlides.length)
+            nextSlideIndex = 0;
+
+        slideList.scrollLeft = projectSlideCenter(nextSlideIndex);
+
+    }
+
+    slideLeftButton.onclick = function () {
+
+        let nextSlideIndex = centralSlideIndex() - 1;
+        if (nextSlideIndex < 0)
+            nextSlideIndex = projectSlides.length - 1;
+
+        slideList.scrollLeft = projectSlideCenter(nextSlideIndex);
+
+    }
+
+    function fadeAndGrowSlidesIntoCenter() {
+        for (let i = 0; i < projectSlides.length; i++) {
+
+            const slideLowerPositionForZeroHeight = projectSlideCenter(i) - SlideDistanceToCenterForZeroHeight();
+            const slideUpperPositionForZeroHeight = projectSlideCenter(i) + SlideDistanceToCenterForZeroHeight();
+
+            const unlerp = (min, max, value) => (value - min) / (max - min);
+
+            const positionBetweenMaxAndMinHeightInPercent = unlerp(slideLowerPositionForZeroHeight, slideUpperPositionForZeroHeight, slideList.scrollLeft);
+            const t = positionBetweenMaxAndMinHeightInPercent;
+            const imgAwayFromItsCenterInPercent = 2 * Math.min(t, 1 - t);
+
+            //slidesImg(i).style.height = maxSlidesImgsHeight * imgAwayFromItsCenterInPercent;
+            slidesImg(i).style.transform = "scale(" + imgAwayFromItsCenterInPercent + ")";
+
+            slidesImg(i).style.opacity = imgAwayFromItsCenterInPercent;
+
+            projectDescription[i].style.opacity = imgAwayFromItsCenterInPercent * 4 - 2;
+        }
+    }
+
+    setInterval(function () {
+        fadeAndGrowSlidesIntoCenter();
+    }, 10);
 });
---
+++ projects.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+        name: "Motion Surfer",
+        description: "Mobile fitness game for iOS and Android that motivates users to move daily through fun and low barriers. Motion Surfer requires no extra hardware and is controlled via AI body-tracking using the front camera of the smartphone.",
+        link: 'https://motionsurfer.de/',
+        image: "images/motionsurfer.png",
+        technologies: ["C#", "Unity", "Firebase", "MediaPipe BlazePose", "OneSignal"],
+    },
+    {
+        name: "AI Handwriting Reader",
+        description: "A web application that displays a selfmodeled AI, which is able to read handwritten numbers and can be trained to read letters by the user.",
+        link: 'https://bryanhogan.github.io/projects/AAI/index.html',
+        image: 'images/ai.png',
+        technologies: ["JavaScript", "HTML", "CSS", "TensorFlow.js"],
+    },
+    {
+        name: "Random Note Generator",
+        description: "A web application that helps musicians train their ear and quickly find the right notes on their instrument.",
+        link: 'https://schwelle76.github.io/Random-Note-Generator/',
+        image: 'images/random-note-generator.png',
+        technologies: ["JavaScript", "HTML", "CSS", "p5.js"],  
+    },
+    {
+        name: "Generative Design",
+        description: "A project showcasing generative design principles and techniques.",
+        link: 'https://mzens.pages.coco.study/startercode-gestaltungsportfolio-2020/',
+        image: 'images/generative-gestaltung.png',
+        technologies: ["JavaScript", "p5.js"],
+    },
+    {
+        name: "Wire Fire",
+        description: "An atmospheric light installation that mimics the flickering glow of a flame.",
+        link: 'https://www.youtube.com/watch?v=7zqv6YlaVhs&ab_channel=Mattes',
+        image: 'images/wirefiretransparentscene.png',
+        technologies: ["C++", "Arduino", "PlatformIO"],
+    }
+
+]
