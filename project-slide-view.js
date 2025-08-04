8b3fdc688529009ff05a1fb5a5ea4a1b70bece9f
Mattes <mattes.zens@web.de>
2025-08-04 18:17:30 +0200

    Slideshow responsiver gemacht und header space oben hinzugefügt

--- project-slide-view.js
+++ project-slide-view.js
@@ -1,42 +1,21 @@
 
+const slideRightButton = document.getElementById('slideRight');
+const slideLeftButton = document.getElementById('slideLeft');
+const slideList = document.getElementsByClassName("projectSlideList")[0];
+const projectSlides = document.getElementsByClassName("projectSlide");
+const projectDescription = document.getElementsByClassName("projectDescription");
 
-document.addEventListener("DOMContentLoaded", function (event) {
+const maxSlidesImgsHeight = 500;
+const SlideDistanceToCenterForZeroHeight = () => window.innerWidth * .8;
 
-    const slideRightButton = document.getElementById('slideRight');
-    const slideLeftButton = document.getElementById('slideLeft');
-    const slideList = document.getElementsByClassName("projectSlideList")[0];
-    const projectSlides = document.getElementsByClassName("projectSlide");
-    const projectDescription = document.getElementsByClassName("projectDescription");
-
-    const maxSlidesImgsHeight = 500;
-    const SlideDistanceToCenterForZeroHeight = () => window.innerWidth * .8;
-
-
-
-
-    function slidesImg(slideIndex) {
-        return projectSlides[slideIndex].getElementsByTagName("img")[0];
-    }
-    
-
-    function projectSlideCenter(slideIndex) {
-        return averageProjectSlideWidth() * slideIndex;
-    }
-    const centralSlideIndex = () => {
-        return Math.round(slideList.scrollLeft / averageProjectSlideWidth())
-    }
-
-    const averageProjectSlideWidth = () => {
-        let totalProjectSlideWidth = 0;
-        for (let i = 0; i < projectSlides.length; i++) {
-            const projectSlideStyle = getComputedStyle(projectSlides[i]);
-            totalProjectSlideWidth += parseFloat(projectSlideStyle.width);
-        }
-
-        return totalProjectSlideWidth / projectSlides.length;
-    }
+window.addEventListener("resize", function(){
+    slideList.scrollLeft = projectSlideCenter(centralSlideIndex());
+});
 
 
+    setInterval(function () {
+        fadeAndGrowSlidesIntoCenter();
+    }, 10);
     slideRightButton.onclick = function () {
 
 
@@ -58,28 +37,48 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     }
 
-    function fadeAndGrowSlidesIntoCenter() {
-        for (let i = 0; i < projectSlides.length; i++) {
 
-            const slideLowerPositionForZeroHeight = projectSlideCenter(i) - SlideDistanceToCenterForZeroHeight();
-            const slideUpperPositionForZeroHeight = projectSlideCenter(i) + SlideDistanceToCenterForZeroHeight();
+function slidesImg(slideIndex) {
+    return projectSlides[slideIndex].getElementsByTagName("a")[0];
+}
 
-            const unlerp = (min, max, value) => (value - min) / (max - min);
 
-            const positionBetweenMaxAndMinHeightInPercent = unlerp(slideLowerPositionForZeroHeight, slideUpperPositionForZeroHeight, slideList.scrollLeft);
-            const t = positionBetweenMaxAndMinHeightInPercent;
-            const imgAwayFromItsCenterInPercent = 2 * Math.min(t, 1 - t);
+function projectSlideCenter(slideIndex) {
+    return averageProjectSlideWidth() * slideIndex;
+}
+const centralSlideIndex = () => {
+    return Math.round(slideList.scrollLeft / averageProjectSlideWidth())
+}
 
-            //slidesImg(i).style.height = maxSlidesImgsHeight * imgAwayFromItsCenterInPercent;
-            slidesImg(i).style.transform = "scale(" + imgAwayFromItsCenterInPercent + ")";
-
-            slidesImg(i).style.opacity = imgAwayFromItsCenterInPercent;
-
-            projectDescription[i].style.opacity = imgAwayFromItsCenterInPercent * 4 - 2;
-        }
+const averageProjectSlideWidth = () => {
+    let totalProjectSlideWidth = 0;
+    for (let i = 0; i < projectSlides.length; i++) {
+        const projectSlideStyle = getComputedStyle(projectSlides[i]);
+        totalProjectSlideWidth += parseFloat(projectSlideStyle.width);
     }
 
-    setInterval(function () {
-        fadeAndGrowSlidesIntoCenter();
-    }, 10);
-});
+    return totalProjectSlideWidth / projectSlides.length;
+}
+
+
+
+function fadeAndGrowSlidesIntoCenter() {
+    for (let i = 0; i < projectSlides.length; i++) {
+
+        const slideLowerPositionForZeroHeight = projectSlideCenter(i) - SlideDistanceToCenterForZeroHeight();
+        const slideUpperPositionForZeroHeight = projectSlideCenter(i) + SlideDistanceToCenterForZeroHeight();
+
+        const unlerp = (min, max, value) => (value - min) / (max - min);
+
+        const positionBetweenMaxAndMinHeightInPercent = unlerp(slideLowerPositionForZeroHeight, slideUpperPositionForZeroHeight, slideList.scrollLeft);
+        const t = positionBetweenMaxAndMinHeightInPercent;
+        const imgAwayFromItsCenterInPercent = 2 * Math.min(t, 1 - t);
+
+        //slidesImg(i).style.height = maxSlidesImgsHeight * imgAwayFromItsCenterInPercent;
+        slidesImg(i).style.transform = "scale(" + imgAwayFromItsCenterInPercent + ")";
+
+        slidesImg(i).style.opacity = imgAwayFromItsCenterInPercent;
+
+        projectDescription[i].style.opacity = imgAwayFromItsCenterInPercent * 4 - 2;
+    }
+}
